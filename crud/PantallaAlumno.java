fd820004f2d932c3d74ea355be4a6adb845dc88f
Jerry <jerry@gmail.com>
2024-03-20 13:04:54 -0600

    Hola

--- crud/PantallaAlumno.java
+++ crud/PantallaAlumno.java
@@ -96,7 +96,7 @@ public class PantallaAlumno extends JFrame {
 
 	public PantallaAlumno() {
 
-		setTitle("CRUD ALUMNO BY RENE dfgsdfgsd");
+		setTitle("CRUD ALUMNO");
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
